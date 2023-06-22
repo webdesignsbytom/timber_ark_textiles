2710d6d1244c12e122e8bf476e7030eeb7a07d45
Thomas Brockington <xtombrock1989@gmail.com>
2023-06-22 18:12:39 +0100

    save

--- client/src/pages/admin/AdminPage.jsx
+++ client/src/pages/admin/AdminPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+// Components
+import Navbar from '../../components/nav/Navbar';
+// Context
+import { ToggleContext } from '../../context/ToggleContext';
 
 function AdminPage() {
+  const { setActiveNav } = useContext(ToggleContext);
+
+  useEffect(() => {
+    setActiveNav('/admin');
+  }, []);
+
   return (
-    <div>AdminPage</div>
-  )
+    <div className='bg-[#f6f6f4] h-screen overflow-hidden'>
+      <section className='grid grid-rows-reg h-full overflow-hidden'>
+        <Navbar />
+        <main className='grid w-full h-full overflow-hidden mt-4'>
+          AdminPage
+        </main>
+      </section>
+    </div>
+  );
 }
 
-export default AdminPage
+export default AdminPage;
